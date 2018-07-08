aff4f258596c8d6679c32025bd945657c10f9f35
chenfengbaobao <15538323378@163.com>
2018-07-08 21:46:15 +0800

    chang to number eight

--- src/main/java/com/how2java/SpringbootApplication.java
+++ src/main/java/com/how2java/SpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 8; i++) {
 			System.out.println(i);
 		}
 
